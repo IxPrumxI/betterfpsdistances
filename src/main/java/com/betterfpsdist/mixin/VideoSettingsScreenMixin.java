4868d83d561045200aca3c88d328398e49d40db2
someaddons <someaddons@github.com>
2024-06-28 21:07:28 +0200

    1.21 port

--- src/main/java/com/betterfpsdist/mixin/VideoSettingsScreenMixin.java
+++ src/main/java/com/betterfpsdist/mixin/VideoSettingsScreenMixin.java
@@ -1,21 +1,26 @@
 package com.betterfpsdist.mixin;
 
 import com.betterfpsdist.event.ClientEventHandler;
-import net.minecraft.client.gui.components.OptionsList;
+import net.minecraft.client.Options;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.gui.screens.options.OptionsSubScreen;
 import net.minecraft.client.gui.screens.options.VideoSettingsScreen;
+import net.minecraft.network.chat.Component;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(VideoSettingsScreen.class)
-public class VideoSettingsScreenMixin
+public abstract class VideoSettingsScreenMixin extends OptionsSubScreen
 {
-    @Shadow
-    private OptionsList list;
 
-    @Inject(method = "init", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/components/OptionsList;addSmall([Lnet/minecraft/client/OptionInstance;)V"))
+    public VideoSettingsScreenMixin(final Screen screen, final Options options, final Component component)
+    {
+        super(screen, options, component);
+    }
+
+    @Inject(method = "addOptions", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/components/OptionsList;addSmall([Lnet/minecraft/client/OptionInstance;)V"))
     public void on(final CallbackInfo ci)
     {
         list.addBig(ClientEventHandler.chunkrenderdist);
