f3fa2a039db81ab3ed65b0b77ba5a9265e96fb73
IxPrumxI <walidaqeel@gmail.com>
2025-09-04 20:57:46 +0300

    This runs, but I have no idea if it's making a difference.

--- src/main/java/com/betterfpsdist/mixin/LevelRendererMixin.java
+++ src/main/java/com/betterfpsdist/mixin/LevelRendererMixin.java
@@ -2,48 +2,50 @@ package com.betterfpsdist.mixin;
 
 import com.betterfpsdist.BetterfpsdistMod;
 import com.betterfpsdist.event.ClientEventHandler;
+import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.client.Camera;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.LevelRenderer;
-import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.chunk.SectionRenderDispatcher;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.SectionPos;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.*;
 
 @Mixin(LevelRenderer.class)
 public class LevelRendererMixin
 {
     @Shadow
     @Final
-    private Minecraft                             minecraft;
-    private SectionRenderDispatcher.RenderSection current = null;
+    private Minecraft minecraft;
 
-    @Redirect(method = "renderSectionLayer", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/chunk/SectionRenderDispatcher$RenderSection;getCompiled()Lnet/minecraft/client/renderer/chunk/SectionRenderDispatcher$CompiledSection;"))
-    public SectionRenderDispatcher.CompiledSection on(final SectionRenderDispatcher.RenderSection instance)
-    {
-        current = instance;
-        return instance.getCompiled();
-    }
+    @ModifyVariable(
+            method = "compileSections",
+            at = @At(
+                    value = "STORE",
+                    ordinal = 0
+            ),
+            ordinal = 0,
+            index = 8
+    )
+    private boolean modifyShouldCompileSection(boolean original, @Local SectionRenderDispatcher.RenderSection renderSection) {
+        if (minecraft.cameraEntity == null) return false;
 
-    @Redirect(method = "renderSectionLayer", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/chunk/SectionRenderDispatcher$CompiledSection;isEmpty(Lnet/minecraft/client/renderer/RenderType;)Z"))
-    public boolean on(final SectionRenderDispatcher.CompiledSection instance, final RenderType type)
-    {
-        if (instance.isEmpty(type))
-        {
-            return true;
-        }
+        BlockPos cameraPos = minecraft.cameraEntity.blockPosition();
+        BlockPos sectionCenter = SectionPos.of(renderSection.getSectionNode()).center();
 
-        if (minecraft.cameraEntity != null && ClientEventHandler.adjustedDistance(minecraft.cameraEntity.blockPosition(), current.getOrigin()) > ClientEventHandler.maxSqDist)
-        {
-            if (BetterfpsdistMod.config.getCommonConfig().debugMode)
-            {
-                ClientEventHandler.hiddenSections.add(current.getOrigin());
+        double distanceSq = sectionCenter.distSqr(cameraPos);
+        if (distanceSq > ClientEventHandler.maxSqDist) {
+            if (BetterfpsdistMod.config.getCommonConfig().debugMode) {
+                ClientEventHandler.hiddenSections.add(sectionCenter);
             }
-            return true;
+            return false;
         }
 
-        return false;
+        return original;
     }
+
 }
